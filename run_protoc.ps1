55c1093aafeabbe2a5b53fcc7588df14af0aead6
RedBrumbler <redbrumbler.official+github@gmail.com>
2023-01-05 17:53:04 +0100

    Update file

--- run_protoc.ps1
+++ run_protoc.ps1
@@ -1,2 +1,3 @@
+# make sure protoc is on your PATH (v3.21.11 was used)
 & protoc --cpp_out=./include --proto_path=./include ./include/SongProto.proto
 move-item ./include/SongProto.pb.cc ./src/SongProto.pb.cc -Force
