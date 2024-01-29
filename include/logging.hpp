59e226bdcf64b25ca09f6dfa52713c4c18dcfb01
RedBrumbler <redbrumbler.official+github@gmail.com>
2024-01-29 13:51:01 +0100

    Make logs tagged

--- include/logging.hpp
+++ include/logging.hpp
@@ -2,8 +2,8 @@
 #include <string_view>
 #include "paper/shared/logger.hpp"
 
-#define INFO(str, ...) Paper::Logger::fmtLog<Paper::LogLevel::INF>(str __VA_OPT__(, __VA_ARGS__))
-#define ERROR(str, ...) Paper::Logger::fmtLog<Paper::LogLevel::ERR>(str __VA_OPT__(, __VA_ARGS__))
-#define CRITICAL(str, ...) Paper::Logger::fmtLog<Paper::LogLevel::ERR>(str __VA_OPT__(, __VA_ARGS__))
-#define DEBUG(str, ...) Paper::Logger::fmtLog<Paper::LogLevel::DBG>(str __VA_OPT__(, __VA_ARGS__))
-#define WARNING(str, ...) Paper::Logger::fmtLog<Paper::LogLevel::WRN>(str __VA_OPT__(, __VA_ARGS__))
+#define INFO(str, ...) Paper::Logger::fmtLogTag<Paper::LogLevel::INF>(str, "SongDetails" __VA_OPT__(, __VA_ARGS__))
+#define ERROR(str, ...) Paper::Logger::fmtLogTag<Paper::LogLevel::ERR>(str, "SongDetails" __VA_OPT__(, __VA_ARGS__))
+#define CRITICAL(str, ...) Paper::Logger::fmtLogTag<Paper::LogLevel::ERR>(str, "SongDetails" __VA_OPT__(, __VA_ARGS__))
+#define DEBUG(str, ...) Paper::Logger::fmtLogTag<Paper::LogLevel::DBG>(str, "SongDetails" __VA_OPT__(, __VA_ARGS__))
+#define WARNING(str, ...) Paper::Logger::fmtLogTag<Paper::LogLevel::WRN>(str, "SongDetails" __VA_OPT__(, __VA_ARGS__))
